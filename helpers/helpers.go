b99cb7e4a71f4f98da3693057829540e2e47070b
javier162380 <javierllorente16@gmail.com>
2020-06-21 10:40:53 +0200

    rename helpers module

--- helpers/helpers.go
+++
@@ -1,9 +0,0 @@
-package helpers
-
-import "github.com/joho/godotenv"
-
-// GenerateEnvFile method to create an env var from a secret service repository
-func GenerateEnvFile(secretdata map[string]string, envpath string) error {
-	return godotenv.Write(secretdata, envpath)
-
-}
